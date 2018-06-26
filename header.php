37e949b6f05e88cfcb66491cf3cc2c825eb410dd
Soumyajit Gorai <soumya173@gmail.com>
2018-06-26 15:14:43 +0530

    Improved design of index
    Added icons in the input fields and improved the layout.

--- header.php
+++ header.php
@@ -1,21 +1,29 @@
 <!-- Navbar -->
 <nav class="navbar navbar-inverse site-header">
-  <div class="container-fluid">
-   <div class="navbar-header">
-     <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#topNavbar">
-       <span class="icon-bar"></span>
-       <span class="icon-bar"></span>
-       <span class="icon-bar"></span>
-     </button>
-     <a class="navbar-brand nav-img" href="index.php"><img src="img/logo.png" alt="" height="20px" width="20px">techAtest</a>
-   </div>
-   <div class="collapse navbar-collapse" id="topNavbar">
-     <!--
-     <ul class="nav navbar-nav navbar-right">
-       <li><a href="login.php"><span class="glyphicon glyphicon-log-in"></span>  Login</a></li>
-       <li><a href="signup.php"><span class="glyphicon glyphicon-plus"></span> Sign Up</a></li>
-     </ul>
-    -->
-   </div>
-  </div>
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#topNavbar">
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand nav-img" href="index.php"><img src="img/logo.png" alt="" height="20px" width="20px">techAtest</a>
+        </div>
+        <div class="collapse navbar-collapse" id="topNavbar">
+
+            <ul class="nav navbar-nav navbar-right">       
+                <li class="dropdown">
+                    <a class="dropdown-toggle" data-toggle="dropdown" href="#"><span class="glyphicon glyphicon-user"></span>&nbsp;&nbsp;Profile
+                    <span class="caret"></span></a>
+                    <ul class="dropdown-menu">
+                        <li><a href="#"><span class="glyphicon glyphicon-pencil"></span>&nbsp;&nbsp;Edit Profile</a></li>
+                        <li><a href="#"><span class="glyphicon glyphicon-signal"></span>&nbsp;&nbsp;Stats</a></li>
+                    </ul>
+                </li>
+                <li><a href="login.php"><span class="glyphicon glyphicon-off"></span>&nbsp;&nbsp;Logout</a></li>
+                <!-- <li><a href="signup.php"><span class="glyphicon glyphicon-plus"></span> Sign Up</a></li> -->
+            </ul>
+
+        </div>
+    </div>
 </nav> <!-- /navbar -->
--- index.php
+++ index.php
@@ -29,23 +29,28 @@
 					</div>
 				</div>
 				<form class="form-horizontal" action="">
-				  <div class="form-group">
-				    <label class="control-label col-sm-2" for="email">Email:</label>
-				    <div class="col-sm-10">
-				      <input type="email" class="form-control" id="email" placeholder="Enter email">
-				    </div>
-				  </div>
-				  <div class="form-group">
-				    <label class="control-label col-sm-2" for="pwd">Password:</label>
-				    <div class="col-sm-10">
-				      <input type="password" class="form-control" id="pwd" placeholder="Enter password">
-				    </div>
-				  </div>
-				  <div class="form-group">
-				    <div class="col-sm-offset-2 col-sm-10">
-				      <button type="submit" class="btn btn-success pull-right">Log In</button>
-				    </div>
-				  </div>
+					<div class="form-group has-feedback">
+						<label class="control-label col-sm-2" for="email">Email:</label>
+						<div class="col-sm-10">
+							<input type="email" class="form-control" id="email" placeholder="Enter email" required>
+							<span class="glyphicon glyphicon-envelope form-control-feedback"></span>
+						</div>
+					</div>
+					<div class="form-group has-feedback">				  	
+						<label class="control-label col-sm-2" for="pwd">Password:</label>
+						<div class="col-sm-10">
+							<input type="password" class="form-control" id="pwd" placeholder="Enter password" required>
+							<span class="glyphicon glyphicon-lock form-control-feedback"></span>
+						</div>
+					</div>
+					<div class="form-group">
+						<div class="col-sm-offset-6 col-sm-4 forgot-pass">
+							<a href="">Forgot Password?</a>
+						</div>
+						<div class="col-sm-2">
+							<button type="submit" class="btn btn-success pull-right"><span class="glyphicon glyphicon-log-in"></span>&nbsp;&nbsp;Log In</button>
+						</div>
+					</div>
 				</form>
 			</div>
 			<div class="col-sm-6 signup-container">
@@ -55,38 +60,57 @@
 					</div>
 				</div>
 				<form class="form-horizontal" action="">
-				  <div class="form-group">
-				    <label class="control-label col-sm-4" for="email">Email:</label>
-				    <div class="col-sm-8">
-				      <input type="email" class="form-control" id="email" placeholder="Enter email">
-				    </div>
-				  </div>
-				  <div class="form-group">
-				    <label class="control-label col-sm-4" for="pwd">Password:</label>
-				    <div class="col-sm-8">
-				      <input type="password" class="form-control" id="pwd" placeholder="Enter password">
-				    </div>
-				  </div>
-				  <div class="form-group">
-				    <label class="control-label col-sm-4" for="pwd">Confirm Password:</label>
-				    <div class="col-sm-8">
-				      <input type="password" class="form-control" id="pwd" placeholder="Enter password">
-				    </div>
-				  </div>
-				  <div class="form-group">
-				    <div class="col-sm-offset-2 col-sm-10">
-				      <button type="submit" class="btn btn-success pull-right">Register</button>
-				    </div>
-				  </div>
+					<div class="form-group has-feedback">
+						<label class="control-label col-sm-4" for="email">Email:</label>
+						<div class="col-sm-8">
+							<input type="email" class="form-control" id="email" placeholder="Enter email" required>
+							<span class="glyphicon glyphicon-envelope form-control-feedback"></span>
+						</div>
+					</div>
+					<div class="form-group has-feedback">
+						<label class="control-label col-sm-4" for="pwd">Password:</label>
+						<div class="col-sm-8">
+							<input type="password" class="form-control" id="pwd" placeholder="Enter password" required>
+							<span class="glyphicon glyphicon-lock form-control-feedback"></span>
+						</div>
+					</div>
+					<div class="form-group has-feedback">
+						<label class="control-label col-sm-4" for="pwd">Confirm Password:</label>
+						<div class="col-sm-8">
+							<input type="password" class="form-control" id="pwd" placeholder="Enter password" required>
+							<span class="glyphicon glyphicon-lock form-control-feedback"></span>
+						</div>
+					</div>
+					<div class="form-group">
+						<label class="control-label col-sm-4" for="pwd">Full Name:</label>
+						<div class="col-sm-4">
+							<input type="text" class="form-control" id="pwd" placeholder="First Name" required>
+						</div>
+						<div class="col-sm-4">
+							<input type="text" class="form-control" id="pwd" placeholder="Last Name" required>
+						</div>
+					</div>
+					<div class="form-group has-feedback">
+						<label class="control-label col-sm-4" for="pwd">Mobile Number:</label>
+						<div class="col-sm-8">
+							<input type="text" class="form-control" id="pwd" placeholder="Enter Mobile Number" required>
+							<span class="glyphicon glyphicon-modal-window form-control-feedback"></span>
+						</div>
+					</div>
+					<div class="form-group">
+						<div class="col-sm-offset-2 col-sm-10">
+							<button type="submit" class="btn btn-success pull-right"> <span class="glyphicon glyphicon-plus"></span>&nbsp;&nbsp;Register</button>
+						</div>
+					</div>
 				</form>
 			</div>
 		</div>
 	</div>
 
-	<div class="container">
+	<div class="container-fluid">
 		<div class="row">
 			<div class="col-sm-12">
-				<h3 class="prop-header">Everything you need to <span>#PracticeBetter</span></h3>
+				<h1 class="prop-header">Everything you need to <span>#PracticeBetter</span></h1>
 			</div>
 		</div>
 		<div class="row prop-row">
@@ -99,7 +123,7 @@
 
 				<div class="row">
 					<div class="col-sm-12">
-						<h3 class="prop-header">Practice</h3>
+						<h2 class="prop-header">Practice</h2>
 					</div>
 				</div>
 
@@ -118,13 +142,13 @@
 
 				<div class="row">
 					<div class="col-sm-12">
-						<h3 class="prop-header">Preapre</h3>
+						<h2 class="prop-header">Preapre</h2>
 					</div>
 				</div>
 
 				<div class="row">
 					<div class="col-sm-12">
-						<p class="sub-header">Prepare yourself<br>with the best<br>study meterials.</p>
+						<p class="sub-header">Prepare<br>yourself with the<br>best study meterials.</p>
 					</div>
 				</div>
 			</div>
@@ -136,13 +160,13 @@
 				</div>
 				<div class="row">
 					<div class="col-sm-12">
-						<h3 class="prop-header">Improve</h3>
+						<h2 class="prop-header">Improve</h2>
 					</div>
 				</div>
 
 				<div class="row">
 					<div class="col-sm-12">
-						<p class="sub-header">Be the<br>better version of<br>yourself everyday.</p>
+						<p class="sub-header">Be the<br>better version<br>of yourself everyday.</p>
 					</div>
 				</div>
 			</div>
